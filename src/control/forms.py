46d01f5f385cb9eb539b122ec50336f10c88c391
Katherine Quijada <katquijadajara@gmail.com>
2024-12-13 06:14:53 -0300

    Corregión Forms

--- src/control/forms.py
+++ src/control/forms.py
@@ -5,13 +5,23 @@ class ingresosform(forms.ModelForm):
     class Meta:
         model = ingresos
         fields = "__all__"
+        widgets = {
+            "fecha_ingreso": forms.DateInput(attrs={"type":"date"}),
+        }
 
 class egresosform(forms.ModelForm):
     class Meta:
         model = egresos
         fields = "__all__"
+        widgets = {
+            "fecha_deuda": forms.DateInput(attrs={"type":"date"}),
+        }
+
 
 class deudaform(forms.ModelForm):
     class Meta:
         model = deuda
         fields = "__all__"
+        widgets = {
+            "fecha_deuda": forms.DateInput(attrs={"type":"date"}),
+        }
